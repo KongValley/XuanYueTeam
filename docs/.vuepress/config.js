e18a857a35d48ee132c8d5d81cb1a1c00d158ea9
KongValley <kzp13968320676@163.com>
2019-06-12 11:38:11 +0800

    docs(Project): 项目初始化
    初步提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,80 @@
+/*
+ * @Author: Chara
+ * @LastEditors: Chara
+ * @Description: config
+ * @Date: 2019-02-15 22:32:00
+ * @LastEditTime: 2019-06-12
+ */
+module.exports = {
+  title: 'Xuan Yue Team',
+  description: 'Xuan Yue Team 内部攻略集合',
+  base: '/XuanYueTeam/',
+  head:[
+    ['link',{ rel: 'icon', href: 'https://blog-chara-img.oss-cn-shanghai.aliyuncs.com/blog-img/20181014154421.ico'}]
+  ],
+  themeConfig: {
+    lastUpdated: '上次更新于',
+    nav: [
+      { 
+        text: '萌新入坑',
+        link: '/guide/'
+      },{
+        text: '攻略集合',
+        items: [
+          { text: '钓鱼', link: '/fishing/' },
+          { text: '龙', link: '/dargon/'},
+          { text: '材料', link: '/material/'}
+        ]
+      },
+      { text: 'GitHub', link: 'https://github.com/KongValley/XuanYueTeam' },
+    ],
+    sidebar: {
+      // '/booknote/': [
+      //   {
+      //     title: '目录',
+      //     collapsable: true,
+      //     children: [
+      //       ''
+      //     ]
+      //   },
+      //   {
+      //     title: '你不知道的JavaScript',
+      //     collapsable: true,
+      //     children: [
+      //       '你不知道的JavaScript/上卷笔记',
+      //       '你不知道的JavaScript/中卷笔记',
+      //       '你不知道的JavaScript/下卷笔记'
+      //     ]
+      //   },
+      //   {
+      //     title: 'JavaScript核心技术开发解密',
+      //     collapsable: true,
+      //     children: [
+      //       'JavaScript核心技术开发解密/第一章'
+      //     ]
+      //   }
+      // ],
+      // '/blog-build/': [
+      //   {
+      //     title: 'Hexo',
+      //     collapsable: true,
+      //     children: [
+      //       '',
+      //       'hexo配置-1',
+      //       'hexo配置-2'
+      //     ]
+      //   },{
+      //     title: 'Vuepress',
+      //     collapsable: true,
+      //     children: [
+            
+      //     ]
+      //   }
+      // ]
+    }
+  },
+  markdown: {
+    toc: { includeLevel: [1, 2, 3] }
+  },
+  plugins: ['@vuepress/back-to-top'] 
+}
---
+++ docs/README.md
@@ -0,0 +1,17 @@
+<!--
+ * @Author: Chara
+ * @Date: 2019-06-12
+ * @LastEditors: Chara
+ * @LastEditTime: 2019-06-12
+ * @Description: 
+ -->
+---
+home: true
+actionText: 开冲 🚀
+actionLink: /xuanyueteam/
+features:
+- title: 一个美服工会
+- title: 会长: Xuan_Yue
+- title: 
+footer: Copyright © 2018-Now Xuan Yue Team
+---
